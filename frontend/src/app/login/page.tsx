58f9613283b3a8ccecd16ed8fb5f0c2c3cff16b0
amarebirhane <amarebirhane89@gmail.com>
2026-03-14 21:04:36 +0300

    started the frontend implementations

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import LoginForm from "@/features/auth/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-50 relative overflow-hidden px-4">
+      {/* Decorative background blobs */}
+      <div className="absolute top-0 -left-4 w-72 h-72 bg-primary-200 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-subtle-pulse"></div>
+      <div className="absolute bottom-0 -right-4 w-72 h-72 bg-primary-300 rounded-full mix-blend-multiply filter blur-3xl opacity-30 animate-subtle-pulse delay-700"></div>
+      
+      <LoginForm />
+    </div>
+  );
+}
